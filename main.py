fb4710bd35921e83c73da3d4f380e8fbf5e89114
yjh52110 <118456391+yjh52110@users.noreply.github.com>
2024-02-07 18:28:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
-FROM_REMOTE=True
+from transformers import AutoModel, AutoTokenizer, AutoModelForCausalLM, LlamaForCausalLM, LlamaTokenizerFast
+from peft import PeftModel  # 0.5.0
 
-base_model = 'llama2'
-peft_model = 'FinGPT/fingpt-mt_llama2-7b_lora' if FROM_REMOTE else 'finetuned_models/MT-llama2-linear_202309210126'
+# Load Models
+base_model = "internlm/internlm-20b" 
+peft_model = "FinGPT/fingpt-sentiment_internlm-20b_lora"
+tokenizer = LlamaTokenizerFast.from_pretrained(base_model, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+model = LlamaForCausalLM.from_pretrained(base_model, trust_remote_code=True, device_map = "cuda:0", load_in_8bit = True,)
+model = PeftModel.from_pretrained(model, peft_model)
+model = model.eval()
 
-model, tokenizer = load_model(base_model, peft_model, FROM_REMOTE)
-
-
-demo_tasks = [
-    'Financial Sentiment Analysis',
-    'Financial Relation Extraction',
-    'Financial Headline Classification',
-    'Financial Named Entity Recognition',
-]
-demo_inputs = [
-    "Glaxo's ViiV Healthcare Signs China Manufacturing Deal With Desano",
-    "Apple Inc Chief Executive Steve Jobs sought to soothe investor concerns about his health on Monday, saying his weight loss was caused by a hormone imbalance that is relatively simple to treat.",
-    'gold trades in red in early trade; eyes near-term range at rs 28,300-28,600',
-    'This LOAN AND SECURITY AGREEMENT dated January 27 , 1999 , between SILICON VALLEY BANK (" Bank "), a California - chartered bank with its principal place of business at 3003 Tasman Drive , Santa Clara , California 95054 with a loan production office located at 40 William St ., Ste .',
-]
-demo_instructions = [
-    'What is the sentiment of this news? Please choose an answer from {negative/neutral/positive}.',
-    'Given phrases that describe the relationship between two words/phrases as options, extract the word/phrase pair and the corresponding lexical relationship between them from the input text. The output format should be "relation1: word1, word2; relation2: word3, word4". Options: product/material produced, manufacturer, distributed by, industry, position held, original broadcaster, owned by, founded by, distribution format, headquarters location, stock exchange, currency, parent organization, chief executive officer, director/manager, owner of, operator, member of, employer, chairperson, platform, subsidiary, legal form, publisher, developer, brand, business division, location of formation, creator.',
-    'Does the news headline talk about price going up? Please choose an answer from {Yes/No}.',
-    'Please extract entities and their types from the input sentence, entity types should be chosen from {person/organization/location}.',
+# Make prompts
+prompt = [
+'''Instruction: What is the sentiment of this news? Please choose an answer from {negative/neutral/positive}
+Input: FINANCING OF ASPOCOMP 'S GROWTH Aspocomp is aggressively pursuing its growth strategy by increasingly focusing on technologically more demanding HDI printed circuit boards PCBs .
+Answer: ''',
+'''Instruction: What is the sentiment of this news? Please choose an answer from {negative/neutral/positive}
+Input: According to Gran , the company has no plans to move all production to Russia , although that is where the company is growing .
+Answer: ''',
+'''Instruction: What is the sentiment of this news? Please choose an answer from {negative/neutral/positive}
+Input: A tinyurl link takes users to a scamming site promising that users can earn thousands of dollars by becoming a Google ( NASDAQ : GOOG ) Cash advertiser .
+Answer: ''',
 ]
 
-test_demo(model, tokenizer)
+# Generate results
+tokens = tokenizer(prompt, return_tensors='pt', padding=True, max_length=512)
+res = model.generate(**tokens, max_length=512)
+res_sentences = [tokenizer.decode(i) for i in res]
+out_text = [o.split("Answer: ")[1] for o in res_sentences]
+
+# show results
+for sentiment in out_text:
+    print(sentiment)
+
+# Output:    
+# positive
+# neutral
+# negative
 
 
 

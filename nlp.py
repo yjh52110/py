83efafc3cc646d301723b5b171c0a686d5cbfb87
yjh52110 <118456391+yjh52110@users.noreply.github.com>
2024-04-19 15:26:08 +0800

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -9,26 +9,21 @@ from transformers import (
 )
 from peft import PeftModel
 
-# Load Models
-base_model = "NousResearch/Llama-2-13b-hf"
-peft_model = "FinGPT/fingpt-sentiment_llama2-13b_lora"
-tokenizer = LlamaTokenizerFast.from_pretrained(base_model, trust_remote_code=True)
-tokenizer.pad_token = tokenizer.eos_token
-model = LlamaForCausalLM.from_pretrained(
-    base_model, trust_remote_code=True, device_map="cuda:0", load_in_8bit=True
-)
-model = PeftModel.from_pretrained(model, peft_model)
-model = model.eval()
-
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
+        print('do_POST')
         content_length = int(self.headers["Content-Length"])
         post_data = self.rfile.read(content_length)
-        input_text = post_data.decode("utf-8")
 
+        # Parse JSON data
+        json_data = json.loads(post_data)
+        input_text = json_data.get("text", "")
+
+        print(input_text)
         # Generate sentiment analysis
         prompt = f"""Instruction: What is the sentiment of this news? Please choose an answer from {{negative/neutral/positive}}
-        Input: {input_text} Answer: """
+        Input: {input_text} 
+        Answer: """
         tokens = tokenizer(prompt, return_tensors="pt", padding=True, max_length=512)
         res = model.generate(**tokens, max_length=512)
         res_sentences = [tokenizer.decode(i) for i in res]
@@ -42,15 +37,24 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(response_data.encode("utf-8"))
 
 
-def run(server_class=HTTPServer, handler_class=RequestHandler, port=8000):
-    server_address = ("", port)
-    httpd = server_class(server_address, handler_class)
-    print(f"Starting server on port {port}...")
-    httpd.serve_forever()
-
+def load_models():
+    base_model = "NousResearch/Llama-2-13b-hf"
+    peft_model = "FinGPT/fingpt-sentiment_llama2-13b_lora"
+    tokenizer = LlamaTokenizerFast.from_pretrained(base_model, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    model = LlamaForCausalLM.from_pretrained(
+        base_model, trust_remote_code=True, device_map="cuda:0", load_in_8bit=True
+    )
+    model = PeftModel.from_pretrained(model, peft_model)
+    model = model.eval()
+    return tokenizer, model
 
 if __name__ == "__main__":
-    run()
+    tokenizer, model = load_models()
+    server_address = ("", 8000)
+    httpd = HTTPServer(server_address, RequestHandler)
+    print("Starting server on port 8000...")
+    httpd.serve_forever()
 
 
 

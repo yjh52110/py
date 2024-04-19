03d41ea6d6c9cf1b31579e71a8248fa01022df79
yjh52110 <118456391+yjh52110@users.noreply.github.com>
2024-04-19 15:37:56 +0800

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -9,31 +9,39 @@ from transformers import (
 )
 from peft import PeftModel
 
+
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        print('do_POST')
+        print("do_POST")
         content_length = int(self.headers["Content-Length"])
         post_data = self.rfile.read(content_length)
 
         # Parse JSON data
         json_data = json.loads(post_data)
-        input_text = json_data.get("text", "")
+        input_texts = json_data.get("text", [])
 
-        print(input_text)
-        # Generate sentiment analysis
-        prompt = f"""Instruction: What is the sentiment of this news? Please choose an answer from {{negative/neutral/positive}}
-        Input: {input_text} 
-        Answer: """
-        tokens = tokenizer(prompt, return_tensors="pt", padding=True, max_length=512)
+        print(input_texts)
+        # Format input texts into prompts
+        prompts = []
+        for input_text in input_texts:
+            prompt = f"""Instruction: What is the sentiment of this news? Please choose an answer from {{negative/neutral/positive}}
+            Input: {input_text}
+            Answer: """
+            prompts.append(prompt)
+
+        # Generate results
+        results = []
+        tokens = tokenizer(prompts, return_tensors="pt", padding=True, max_length=512)
         res = model.generate(**tokens, max_length=512)
         res_sentences = [tokenizer.decode(i) for i in res]
         out_text = [o.split("Answer: ")[1] for o in res_sentences]
+        results.extend(out_text)
 
         # Prepare response
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
-        response_data = json.dumps({"sentiment": out_text[0]})
+        response_data = json.dumps({"sentiments": results})
         self.wfile.write(response_data.encode("utf-8"))
 
 
@@ -49,12 +57,11 @@ def load_models():
     model = model.eval()
     return tokenizer, model
 
+
 if __name__ == "__main__":
     tokenizer, model = load_models()
     server_address = ("", 8000)
     httpd = HTTPServer(server_address, RequestHandler)
     print("Starting server on port 8000...")
     httpd.serve_forever()
-
-
 
